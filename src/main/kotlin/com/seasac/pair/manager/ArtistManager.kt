d40c6428e230c866dbea19aee3ded8fcee3d94d3
harry <harry7408@naver.com>
2023-09-16 21:51:04 +0900

    refactor : 코드 수정

--- src/main/kotlin/com/seasac/pair/manager/ArtistManager.kt
+++ src/main/kotlin/com/seasac/pair/manager/ArtistManager.kt
@@ -3,7 +3,6 @@ package com.seasac.pair.manager
 import com.seasac.pair.Entity.Artist
 import com.seasac.pair.FeatureInterface
 import com.seasac.pair.common.ConsoleReader
-import com.seasac.pair.common.Genre
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
@@ -11,12 +10,10 @@ import java.io.*
 
 class ArtistManager() : FeatureInterface {
 
-    var artistList: MutableList<Artist> = mutableListOf()
-
-
+    val artistList: MutableList<Artist> = mutableListOf()
 
     override fun <T> update(t: T) {
-        updateSerializationArtistFile()
+
 
     }
 
@@ -57,7 +54,7 @@ class ArtistManager() : FeatureInterface {
                 artistList.removeAt(index)
             }
         }
-        if (currentListCount - 1 == artistList.size ) {
+        if (currentListCount - 1 == artistList.size) {
             println("삭제 완료되었습니다")
         } else {
             println("다시 입력해주세요")
@@ -65,55 +62,9 @@ class ArtistManager() : FeatureInterface {
         }
     }
 
+    companion object {
 
-    /**
-     * 파일에서 읽어 리스트에 저장하는 함수
-     */
-    fun takeSerializationArtistFile() = runBlocking {
-        artistList = withContext(Dispatchers.IO){
-            ObjectInputStream(FileInputStream("./serialization/artist.ser")).use {
-                arrayListOf<Artist>().apply{
-                    while (true){
-                        try {
-                            add(it.readObject() as Artist)
-                        }catch (e : EOFException){
-                            break
-                        }
-                    }
-                }
-            }
-        }
-        println(artistList)
     }
-
-
-
-
-    /**
-     * 파일을 새로 업데이트하는 함수
-     */
-    fun updateSerializationArtistFile() = runBlocking {
-
-            var artistName  = ConsoleReader.consoleLineScanner()
-            var genre = ConsoleReader.consoleLineScanner()
-            when(genre){
-
-            }
-
-            enroll(Artist(artistName, Genre.BALLAD,""))
-            val message = withContext(Dispatchers.IO){
-                ObjectOutputStream(FileOutputStream("./serialization/artist.ser")).use {
-                    with(it){
-                        writeObject(artistList)
-                        flush()
-                    }
-                }
-            }
-            println(message)
-    }
-
-
-
 }
 
 
