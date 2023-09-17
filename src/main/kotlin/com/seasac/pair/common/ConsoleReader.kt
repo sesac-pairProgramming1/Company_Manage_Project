26f1899268bdc26b51daa8687bce2c75d5942f93
harry <harry7408@naver.com>
2023-09-17 17:42:05 +0900

    style : 일부 주석 추가

--- src/main/kotlin/com/seasac/pair/common/ConsoleReader.kt
+++ src/main/kotlin/com/seasac/pair/common/ConsoleReader.kt
@@ -2,6 +2,11 @@ package com.seasac.pair.common
 
 import java.util.Scanner
 
+/**
+ * Console reader
+ * Scanner 는 객체 하나만 생성되는 것이 좋다
+ * @constructor Create empty Console reader
+ */
 class ConsoleReader {
     companion object {
         private lateinit var scanner : Scanner
